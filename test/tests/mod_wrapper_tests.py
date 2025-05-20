2e612d8236fbf6b7ca2b6878dc392beef3df30d1
hwikle-lanl <154543628+hwikle-lanl@users.noreply.github.com>
2025-05-20 10:50:04 -0600

    817 automatically perform module purge before building tests (#834)
    * Perform module purge before building
    
    * Various changes
    
    * Purge modules before building and running
    
    * Only perform module purge when modules are specified
    
    * Add argument to disable module purging
    
    * Add purge_modules option to build and run
    
    * Revert commits on wrong branch
    
    * Remove module purge cmd line arg
    
    * Make module purge unit test more robust
    
    * Made some progress
    
    * Fix module purge in build/run script
    
    * Fix module purge run test
    
    * Fix module purge build unit test
    
    * Fix log test
    
    * Skip module purge tests when no module syste
    
    * Attempt to fix module purge
    
    * Fix log command test
    
    * Fix purging behavior in unit tests (maybe)

--- test/tests/mod_wrapper_tests.py
+++ test/tests/mod_wrapper_tests.py
@@ -321,3 +321,61 @@ class ModWrapperTests(PavTestCase):
         for mod, result in checks:
             self.assertEqual(module_wrapper.parse_module(mod), result)
 
+    @unittest.skipIf(not has_module_cmd() and find_module_init() is None,
+                     "Could not find a module system.")
+    def test_run_module_purge(self):
+        """Test that a module purge is performed when running tests."""
+
+        # Test that a module purge is performed by default
+        test_cfg = self._quick_test_cfg()
+        test_cfg['run']['cmds'] = [
+            '[[ $(module -t list 2>&1) = "No modules loaded" ]] || exit 1',
+        ]
+        test_cfg['run']['preamble'].append('module load test_mod1 || exit 2')
+
+        test = self._quick_test(test_cfg)
+
+        run_result = test.run()
+
+        self.assertEqual(run_result, 0)
+
+        # Check that we can disable purging
+        test_cfg = self._quick_test_cfg()
+        test_cfg['run']['cmds'] = [
+            '[[ $(module -t list 2>&1) = "No modules loaded" ]] || exit 1',
+        ]
+        test_cfg['run']['preamble'].append('module load test_mod1 || exit 2')
+        test_cfg["run"]["purge_modules"] = False
+        test = self._quick_test(test_cfg)
+        run_result = test.run()
+
+        self.assertEqual(run_result, 1)
+
+    @unittest.skipIf(not has_module_cmd() and find_module_init() is None,
+                     "Could not find a module system.")
+    def test_build_module_purge(self):
+        """Test that a module purge is performed when building tests."""
+
+        # Test that a module purge is performed by default
+        test_cfg = self._quick_test_cfg()
+        test_cfg['build']['cmds'] = [
+            '[[ $(module -t list 2>&1) = "No modules loaded" ]] || exit 1',
+        ]
+        test_cfg['run']['preamble'].append('module load test_mod1 || exit 2')
+
+        test = self._quick_test(test_cfg, build=False)
+        build_result = test.build()
+
+        self.assertEqual(build_result, True)
+
+        # Check that we can disable purging
+        test_cfg = self._quick_test_cfg()
+        test_cfg['build']['cmds'] = [
+            '[[ $(module -t list 2>&1) = "No modules loaded" ]] || exit 1',
+        ]
+        test_cfg['build']['preamble'] = ['module load test_mod1 || exit 2']
+        test_cfg["build"]["purge_modules"] = False
+        test = self._quick_test(test_cfg, build=False)
+        build_result = test.build()
+
+        self.assertEqual(build_result, False)
